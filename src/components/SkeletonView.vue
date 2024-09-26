6641c5df1e9846fe5d1b18557ed3bcea991529eb
amufid <91891129+amufid@users.noreply.github.com>
2024-09-26 08:51:25 +0700

    fix page and add skeleton

---
+++ src/components/SkeletonView.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="skeleton-container">
+      <div class="skeleton skeleton-image"></div>
+      <div>
+         <div class="skeleton skeleton-text"></div>
+         <div class="skeleton skeleton-text"></div>
+         <div class="skeleton skeleton-text__body"></div>
+         <div class="skeleton skeleton-text__body"></div>
+         <div class="skeleton skeleton-text__body"></div>
+         <div class="skeleton skeleton-text__body"></div>
+         <div class="skeleton-group-button">
+            <div class="skeleton skeleton-footer"></div>
+            <div class="skeleton skeleton-footer"></div>
+         </div>
+      </div>
+   </div>
+</template>
